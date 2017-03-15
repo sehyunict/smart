b5ccfa5bea24265fd2a850d2d293660000e82dc2
zzinpan <12y8e@naver.com>
2017-03-15 11:51:17 +0900

    JAVA format 추가.

---
+++ codeConvention/java/Test.java
@@ -0,0 +1,41 @@
+public class Test {
+	// @formatter:off
+	public static void test1(String[] args0, char args1){
+		
+		String 				aaa = "aaa";
+		String bbb = null;
+		Integer no;
+		String 		ccccc = "cccc";
+		char 	d = 'd';
+		String 		e;
+		
+		
+		if(bbb == "1"){
+			
+		}else if(d=='c'){
+			
+		}
+		
+	}
+	// @formatter:on
+
+	// ctrl + shift + f
+	public static void test2(String[] args0, char args1) {
+
+		String 				aaa = "aaa";
+		String bbb = null;
+		Integer no;
+		String 		ccccc = "cccc";
+		char 	d = 'd';
+		String 		e;
+		
+		
+		if(bbb == "1"){
+			
+		}else if(d=='c'){
+			
+		}
+
+	}
+
+}
